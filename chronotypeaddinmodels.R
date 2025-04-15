d6473be8ad0d5faf3e10e6492095030a9a25bb90
Robert Maidstone <robert.maidstone@manchester.ac.uk>
2025-04-15 16:18:50 +0100

    Create chronotypeaddinmodels.R

---
+++ chronotypeaddinmodels.R
@@ -0,0 +1,100 @@
+
+library(tidyverse)
+library(officer)
+library(flextable)
+library(lubridate)
+library(lmtest)
+library(patchwork)
+
+source("load_data.R")
+source("ORmodelfunctions.R")
+source("ORdatawrangling.R")
+
+rm(ukb_data_processed)
+
+tf<-function(table_input){
+  table_input %>% mutate(samplesize=`FALSE`+`TRUE`,trueprop=round(`TRUE`/samplesize*100,2)) %>% 
+    dplyr::select(JiNS,`TRUE`,trueprop,samplesize,everything()) %>% dplyr::select(-`FALSE`) %>% t
+}
+
+sex_plots <- function(table_all,table_men,table_women,title,model){
+  rbind(
+    table_all %>% pivot_longer(cols=4:9,names_to = "Model",values_to = "values") %>% dplyr::select(-`FALSE`,-`TRUE`) %>% mutate(group="all"),
+    table_women %>% pivot_longer(cols=4:9,names_to = "Model",values_to = "values") %>% dplyr::select(-`FALSE`,-`TRUE`) %>% mutate(group="women"),
+    table_men %>% pivot_longer(cols=4:9,names_to = "Model",values_to = "values") %>% dplyr::select(-`FALSE`,-`TRUE`) %>% mutate(group="men")
+  ) %>%
+    as_tibble %>% 
+    filter(group!="all") %>%
+    separate(values,sep = "[ |-]",into=c("OR","LCI","UCI")) %>%
+    mutate(LCI=as.numeric(str_remove(LCI,pattern = "\\("))) %>%
+    mutate(UCI=as.numeric(str_remove(UCI,pattern = "\\)"))) %>%
+    mutate(OR=as.numeric(OR)) %>%
+    mutate(OR=ifelse(is.na(OR),1,OR)) %>%
+    mutate(LCI=ifelse(is.na(OR),NULL,LCI)) %>%
+    mutate(UCI=ifelse(is.na(OR),NULL,UCI)) %>%
+    separate(Model,into="Model",sep = ":") %>%
+    mutate(Model=factor(Model,levels=c("Model 7","Model 6","Model 5","Model 4","Model 3a","Model 3","Model 2a","Model 2","Model 1a","Model 1"))) %>%
+    mutate(group=factor(group,levels=c("women","men","all"))) %>%
+    mutate(JiNS=factor(JiNS,levels=c("No shift work","Never/rarely","Irregular shift work","Always"),labels=c("Day workers","Shift work, but never\nor rarely nights","Irregular shift work\nincluding nights","Permanent night\nshift work"))) -> plot_data
+  
+  pd_width <- 0.6
+  
+  plot_data %>%
+    filter(Model==model) %>%
+    ggplot(aes(y=OR,x=JiNS,colour=group)) + 
+    geom_hline(aes(yintercept = 1), size = .25, linetype = "dashed") +
+    geom_errorbar(aes(ymax = UCI, ymin = LCI), size = .5, width = .4,
+                  position = position_dodge(width = pd_width)) +
+    geom_point(position = position_dodge(width = pd_width)) + 
+    theme_bw() +
+    theme(axis.title.y = element_blank(),
+          legend.position=c(0.8, 0.8),
+          legend.background = element_blank())+
+    ylab("Odds Ratio") +
+    #ylim(c(.6,2)) +
+    coord_flip() +
+    scale_x_discrete(limits = rev(levels(plot_data$JiNS)))+
+    scale_colour_manual(#values = cbPalette[2:4],
+      #values = c("transparent","transparent","black"),
+      values = c("black","red"),
+      name = element_blank(),guide = guide_legend(reverse=TRUE)) +
+    ggtitle(title) -> plot_ms2
+  
+  return(plot_ms2)
+}
+
+# JiNS -----------------------------------------------------------------
+
+model_vec<-c("JiNS + Year_of_birth",
+             "JiNS + Chronotype_o + Year_of_birth",
+             #"JiNS + Testosterone + JiNS*Testosterone + Year_of_birth",
+             "JiNS + Year_of_birth + Ethnicity_o + TDI + Alcohol + Alcintake + DaysWalked + DaysModerate + DaysVigorous  + LengthofWW_o + Job_AsthmaRisk + Job_MedRequired",
+             "JiNS + Chronotype_o + Year_of_birth + Ethnicity_o + TDI + Alcohol + Alcintake + DaysWalked + DaysModerate + DaysVigorous  + LengthofWW_o + Job_AsthmaRisk + Job_MedRequired",
+             # "JiNS + Testosterone + JiNS*Testosterone + Year_of_birth + Ethnicity_o + TDI + Alcohol + Alcintake + DaysWalked + DaysModerate + DaysVigorous  + LengthofWW_o + Job_AsthmaRisk + Job_MedRequired + Chronotype_o",
+             "JiNS + Year_of_birth + Ethnicity_o + TDI + Alcohol + Alcintake + DaysWalked + DaysModerate + DaysVigorous  + LengthofWW_o + Job_AsthmaRisk + Job_MedRequired +Smoking_n + Packyears_nn + BMI_o + SleepDur +Sleep_med",
+             # "JiNS + Testosterone + JiNS*Testosterone + Year_of_birth + Ethnicity_o + TDI + Alcohol + Alcintake + DaysWalked + DaysModerate + DaysVigorous  + LengthofWW_o + Job_AsthmaRisk + Job_MedRequired + Chronotype_o+Smoking_n + Packyears_nn + BMI_o + SleepDur",
+             "JiNS + Chronotype_o + Year_of_birth + Ethnicity_o + TDI + Alcohol + Alcintake + DaysWalked + DaysModerate + DaysVigorous  + LengthofWW_o + Job_AsthmaRisk + Job_MedRequired +Smoking_n + Packyears_nn + BMI_o + SleepDur +Sleep_med")
+model_names <- c("Model 1",
+                 "Model 1a: Model 1 + chronotype",
+                 "Model 2",
+                 "Model 2a: Model 2 + chronotype",
+                 "Model 3",
+                 "Model 3a: Model 3 + chronotype")
+
+model_data  %>%
+  filter(!is.na(Packyears_nn)) %>%
+  mutate(JiNS=JiNS_o) %>%
+  filter((Asthma_med_all == FALSE&Asthma2==FALSE) | (Asthma_def_ms==TRUE))-> model_data_temp
+DependentVar <- "Asthma_def_ms"
+
+#### filter out people with no hormone data
+load(file="data/hormonequartiledata.RData")
+
+ORmodelrun_4shift(model_data_temp%>%filter(Sex==1),DependentVar,model_vec,model_names)[[1]] -> tab7asthmadefms_men
+
+ORmodelrun_4shift(model_data_temp%>%filter(Sex==0),DependentVar,model_vec,model_names)[[1]] -> tab7asthmadefms_women
+
+sex_plots(tab7asthmadefms_men,tab7asthmadefms_men,tab7asthmadefms_women,"B.","Model 2") + guides(colour="none")  + ylab("Odds ratio of moderate-\nsevere asthma") +theme(plot.title.position = "plot")+
+  sex_plots(tab7asthmadefms_men,tab7asthmadefms_men,tab7asthmadefms_women,"","Model 2a") + ggtitle("", subtitle="additionally controlled for\nchronotype") + theme(axis.text.y = element_blank(),axis.ticks.y = element_blank())+ ylab("Odds ratio of moderate-\nsevere asthma") -> p3
+
+ggsave(filename="plots/hormones_additionallycontrolledbychrono.png",plot = p3,width=6,height=4)
